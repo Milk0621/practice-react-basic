61462e50bf63d6adccddad1caf03e27138c6020b
Milk0621 <wltjs5360@gmail.com>
2025-06-30 21:21:17 +0900

    feat: 게시글 목록 및 좋아요 버튼 기능 구현
    - useState 배열을 사용하여 3개의 게시글 제목을 출력
    - onClick 이벤트와 따봉 state를 활용해 좋아요 기능 구현
    - 기초적인 React state와 이벤트 바인딩 학습 목적

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,24 @@ import './App.css'
 function App() {
 
   let post = '강남 우동 맛집'
-  let [글제목,b] = useState('남자 코트 추천');
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
+  let [따봉, 따봉변경] = useState(0);
 
   return (
     <div className='App'>
       <div className='black-nav'>
-        <h4 style={ { color : 'yellow', fontSize : '16px' } }>블로그임</h4>
+        <h4>ReactBlog</h4>
       </div>
       <div className='list'>
-        <h4>{ 글제목 }</h4>
+        <h4>{ 글제목[0] } <span onClick={ ()=>{ 따봉변경(따봉+1) } }>👍</span> { 따봉 } </h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{ 글제목[1] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{ 글제목[2] }</h4>
         <p>2월 17일 발행</p>
       </div>
     </div>
