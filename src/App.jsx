d60b9db50ae3246af73950c47eb0bb6d674fb873
Milk0621 <wltjs5360@gmail.com>
2025-06-30 20:09:44 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  let post = '강남 우동 맛집'
+  let [글제목,b] = useState('남자 코트 추천');
+
+  return (
+    <div className='App'>
+      <div className='black-nav'>
+        <h4 style={ { color : 'yellow', fontSize : '16px' } }>블로그임</h4>
+      </div>
+      <div className='list'>
+        <h4>{ 글제목 }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
