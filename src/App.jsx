970bd2b25aea18291fbe12dd751145766e4b4700
Milk0621 <wltjs5360@gmail.com>
2025-07-01 11:00:59 +0900

    feat: 모달 컴포넌트 생성 및 조건부 렌더링으로 표시 기능 구현
    - Modal 컴포넌트를 따로 분리하여 UI 재사용 구조로 작성
    - useState로 모달의 on/off 상태 제어
    - 글 제목 클릭 시 모달 토글 기능 구현
    - 조건부 렌더링을 통해 동적인 UI 학습 목적

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   let post = '강남 우동 맛집'
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
   let [따봉, 따봉변경] = useState(0);
+  let [modal, setModal] = useState(false);
 
   return (
     <div className='App'>
@@ -17,13 +18,28 @@ function App() {
         <p>2월 17일 발행</p>
       </div>
       <div className='list'>
-        <h4>{ 글제목[1] }</h4>
+        <h4 onClick={ ()=>{ setModal(!modal) } }>{ 글제목[1] }</h4>
         <p>2월 17일 발행</p>
       </div>
       <div className='list'>
         <h4>{ 글제목[2] }</h4>
         <p>2월 17일 발행</p>
       </div>
+
+      {
+        modal == true ? <Modal/> : null  
+      }
+    
+    </div>
+  )
+}
+
+function Modal(){
+  return (
+    <div className='modal'>
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
     </div>
   )
 }
